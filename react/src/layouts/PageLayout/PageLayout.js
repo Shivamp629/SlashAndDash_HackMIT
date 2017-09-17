181a18001116710f90a928075b70916b76d5ae20
Janki Patel <jpatel315@gatech.edu>
2017-09-17 01:49:28 -0400

    changing home page

--- react/src/layouts/PageLayout/PageLayout.js
+++ react/src/layouts/PageLayout/PageLayout.js
@@ -3,15 +3,18 @@ import { IndexLink, Link } from 'react-router'
 import PropTypes from 'prop-types'
 import './PageLayout.scss'
 
+
 export const PageLayout = ({ children }) => (
   <div className='container text-center'>
-    <h1>React Redux Starter Kit</h1>
-    <IndexLink to='/' activeClassName='page-layout__nav-item--active'>Home</IndexLink>
-    {' · '}
-    <Link to='/counter' activeClassName='page-layout__nav-item--active'>Counter</Link>
-    <div className='page-layout__viewport'>
-      {children}
-    </div>
+    <h1>Slash and Dash</h1>
+    {/*<Link to='/counter' activeClassName='page-layout__nav-item--active'>Search</Link>*/}
+    {/*<div className='page-layout__viewport'>*/}
+      {/*{children}*/}
+    {/*</div>*/}
+    <search>
+        <input type="text" name="search"/>
+        <button onClick={fetch()}>Search</button>
+    </search>
   </div>
 )
 PageLayout.propTypes = {
