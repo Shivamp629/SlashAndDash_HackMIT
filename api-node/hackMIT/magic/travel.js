50290df68ab5f3ca110fe39571bfd431bfca0009
Shivamp629 <shivamp629@gmail.com>
2017-09-17 07:24:09 -0400

    Python struggles

--- api-node/hackMIT/magic/travel.js
+++ api-node/hackMIT/magic/travel.js
@@ -1,9 +1,14 @@
-var PythonShell = require('python-shell');
 
 
-module.exports.flying = function (name) {
-    PythonShell.run('Amadeus.py', options, function (err, results) {
-        console.log(results);
+module.exports.flying = function (name, passengers) {
+    console.log("Hello");
+    var spawn = require("child_process").spawn;
+    var process = spawn('python',["path/to/script.py", arg1, arg2]);
+    console.log("Hello2");
+    process.stdout.on('data', function (data){
+        console.log("Hello3");
+        console.log(data);
+        console.log("Hello");
     });
-
+    console.log("Hello4");
 }
